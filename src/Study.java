62f37cb04779bb61dbc9ff59dbd9d77fbbc1b562
Pavel <pawel.wowniuk@g,ail.com>
2019-01-09 13:58:07 +0100

    Stwożyłem maina oraz plik gitignore

---
+++ src/Study.java
@@ -0,0 +1,8 @@
+public class Study {
+    public static void main(String[] args) {
+        System.out.println("Ucze");
+        System.out.println("sie");
+        System.out.println("programowania");
+
+    }
+}
